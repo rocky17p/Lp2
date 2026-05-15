a1883ce0f7fcaba9a6c437ac548afdfef575462f
RISHI PATWA <rishi17p@gmail.com>
2026-05-15 16:54:46 +0530

    Update a*.cpp

--- a*.cpp
+++ a*.cpp
@@ -5,90 +5,104 @@ int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
 
 // Manhattan Distance
-int h(int x, int y, int gx, int gy) { return abs(x - gx) + abs(y - gy); }
-
-bool valid(int x, int y, vector<vector<int>> &g) {
-  return x >= 0 && y >= 0 && x < g.size() && y < g[0].size() && g[x][y] == 0;
+int h(int x, int y, int gx, int gy) {
+    return abs(x - gx) + abs(y - gy);
 }
 
-void AStar(vector<vector<int>> &grid, pair<int, int> s, pair<int, int> g) {
-  if (grid[s.first][s.second] == 1 || grid[g.first][g.second] == 1) {
-    cout << "Start or Goal is blocked\n";
-    return;
-  }
-  int n = grid.size(), m = grid[0].size();
+bool valid(int x, int y, vector<vector<int>> &grid) {
+    return x >= 0 && y >= 0 &&
+           x < grid.size() &&
+           y < grid[0].size() &&
+           grid[x][y] == 0;
+}
 
-  vector<vector<int>> cost(n, vector<int>(m, INT_MAX));
-  vector<vector<pair<int, int>>> parent(n, vector<pair<int, int>>(m));
+void AStar(vector<vector<int>> &grid, int sx, int sy, int gx, int gy) {
 
-  priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>>
-      pq;
-  // (f, x, y)
-
-  cost[s.first][s.second] = 0;
-  pq.push({h(s.first, s.second, g.first, g.second), s.first, s.second});
-
-  while (!pq.empty()) {
-    auto [f, x, y] = pq.top();
-    pq.pop();
-
-    if (x==g.first && y==g.second) {
-      cout << "\n\n Path: ";
-      vector<pair<int, int>> path;
-
-      while (!(x == s.first && y == s.second)) {
-        path.push_back({x, y});
-
-        int px = parent[x][y].first;
-        int py = parent[x][y].second;
-
-        x = px;
-        y = py;
-      }
-      path.push_back(s);
-      reverse(path.begin(), path.end());
-
-      for (auto p : path)
-        cout << "(" << p.first << "," << p.second << ") ";
-
-      cout << endl << endl;
-
-      return;
+    // Check start and goal
+    if (grid[sx][sy] == 1 || grid[gx][gy] == 1) {
+        cout << "Start or Goal is blocked\n";
+        return;
     }
 
-    for (int i = 0; i < 4; i++) {
-      int nx = x + dx[i], ny = y + dy[i];
+    int n = grid.size();
+    int m = grid[0].size();
 
-      if (valid(nx, ny, grid)) {
-        int newCost = cost[x][y] + 1;
+    vector<vector<int>> cost(n, vector<int>(m, INT_MAX));
+    vector<vector<pair<int, int>>> parent(n,vector<pair<int, int>>(m));
 
-        if (newCost < cost[nx][ny]) {
-          cost[nx][ny] = newCost;
-          parent[nx][ny] = {x, y};
+    priority_queue<tuple<int, int, int>,vector<tuple<int, int, int>>,greater<>> pq;
 
-          int hVal = h(nx, ny, g.first, g.second);
-          int fVal = newCost + hVal;
+    // (f, x, y)
 
-          cout << "\n Node: (" << nx << "," << ny << ") ";
-          cout << "\n g(n)=" << newCost << " ";
-          cout << "\n h(n)=" << hVal << " ";
-          cout << "\n f(n)=" << fVal << endl;
+    cost[sx][sy] = 0;
+    pq.push({h(sx, sy, gx, gy), sx, sy});
 
-          pq.push({fVal, nx, ny});
+    while (!pq.empty()) {
+        auto [f, x, y] = pq.top();
+        pq.pop();
+
+        // Goal reached
+        if (x == gx && y == gy) {
+            cout << "\n\nPath: ";
+
+            vector<pair<int, int>> path;
+
+            while (!(x == sx && y == sy)) {
+                path.push_back({x, y});
+
+                int px = parent[x][y].first;
+                int py = parent[x][y].second;
+
+                x = px;
+                y = py;
+            }
+
+            path.push_back({sx, sy});
+            reverse(path.begin(), path.end());
+
+            for (auto p : path) {
+                cout << "(" << p.first << "," << p.second << ") ";
+            }
+
+            cout << endl << endl;
+            return;
         }
-      }
-    }
-  }
 
-  cout << "No path found\n";
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (valid(nx, ny, grid)) {
+                int newCost = cost[x][y] + 1;
+
+                if (newCost < cost[nx][ny]) {
+                    cost[nx][ny] = newCost;
+                    parent[nx][ny] = {x, y};
+
+                    int hVal = h(nx, ny, gx, gy);
+                    int fVal = newCost + hVal;
+
+                    cout << "\nNode: (" << nx << "," << ny << ")";
+                    cout << "\ng(n) = " << newCost;
+                    cout << "\nh(n) = " << hVal;
+                    cout << "\nf(n) = " << fVal << endl;
+
+                    pq.push({fVal, nx, ny});
+                }
+            }
+        }
+    }
+
+    cout << "No path found\n";
 }
 
 int main() {
-  vector<vector<int>> grid = {
-    {1, 0, 0, 0}, 
-    {0, 1, 1, 0}, 
-    {0, 1, 0, 0}, 
-    {0, 0, 0, 0}};
+    vector<vector<int>> grid = {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0}
+    };
 
-  AStar(grid, {0, 0}, {3, 2});
+    AStar(grid, 0, 0, 3, 2);
 }
